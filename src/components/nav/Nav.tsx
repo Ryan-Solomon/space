212d2fcd6ac6f8ca0ec91f26abae4d6e0f04387a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-06 20:21:12 -0600

    add nav bp

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,11 @@
+export const Nav = () => {
+  return (
+    <nav className='nav'>
+      <ul>
+        <li>Mars Rover</li>
+        <li>Earth</li>
+        <li>Near-Earth Asteroids</li>
+      </ul>
+    </nav>
+  );
+};
